8f7373a9541b980d2ecdf05090d505750263d5a7
YehezkiEl1404 <164315594+YehezkiEl1404@users.noreply.github.com>
2024-04-22 13:39:00 +0800

    Add files via upload

---
+++ Stack (2).cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+class stack
+{
+	public:
+		int size = 0;
+		int top = -1;
+		char *items;
+		
+		stack(int stackSize)
+		{
+			size = stackSize;
+			items = new char[size];
+		}
+		void push(char item)
+		{
+			if (top ==size -1)
+			{
+				cout << "ERROR: Stack overflow, stack is full" << endl;
+			}
+			else 
+			{
+				top++;
+				items[top] = item;
+			}
+		}
+		char pop()
+		{
+			char item;
+			if (top==-1)
+			{
+				cout << "ERROR: Stack is empty" << endl;
+			}
+			else
+			{
+				item = items[top];
+				top--;
+			}
+			return item;
+		}
+
+};
+
+
+
+int main(int argc, char** argv) 
+{
+	stack kantong1('5');
+	
+	kantong1.push('A');
+	kantong1.push('B');
+	kantong1.push('C');
+	kantong1.push('D');
+	kantong1.push('Z');
+	kantong1.push('R');
+	
+	cout << kantong1.pop() << endl;
+	cout << kantong1.pop() << endl;
+	cout << kantong1.pop() << endl;
+	cout << kantong1.pop() << endl;
+	cout << kantong1.pop() << endl;
+	cout << kantong1.pop() << endl;
+	return 0;
+}
